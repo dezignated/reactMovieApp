bdf7760660be1ee435df0d17eed108463f476d04
Desiree Godwin <dcgodwin5@gmail.com>
2020-08-08 19:00:16 -0600

    connected netlify

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchMovies from './SearchMovies';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">React Movie Search</h1>
+      <h1 className="title">React Movie Search!</h1>
       <SearchMovies />
     </div>
   )
